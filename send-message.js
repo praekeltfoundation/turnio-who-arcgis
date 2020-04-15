061c319823e8aed3325abf4d755a1b945a5aca65
Simon de Haan <simon@praekelt.org>
2020-04-15 12:15:12 +0200

    use debug for logging

--- send-message.js
+++ send-message.js
@@ -1,27 +1,27 @@
-const axios = require('axios').default
+const axios = require("axios").default;
+const debug = require("debug")("turn");
 
-const ENVIRONMENT = process.env.ENVIRONMENT || 'dev'
-const conf = require(`./conf.${ENVIRONMENT}.json`)
+const TOKEN = process.env.TOKEN;
 
-const turnMsg = `https://whatsapp.turn.io/v1/messages`
-module.exports = function sendMessage (claimUuid, body, to) {
+const turnMsg = `https://whatsapp.turn.io/v1/messages`;
+module.exports = function sendMessage(messageId, body, to) {
+  debug(`sending message to ${to} in reply to ${messageId}`);
   return axios({
-    method: 'post',
+    method: "post",
     url: turnMsg,
     headers: {
-      "Authorization": `Bearer ${conf.token}`,
-      "X-Turn-Claim-Extend": claimUuid,
+      Authorization: `Bearer ${TOKEN}`,
+      "X-Turn-In-Reply-To": messageId,
       "Content-Type": "application/json",
     },
     body: {
-      "preview_url": false,
-      "recipient_type": "individual",
-      "to": to,
-      "type": "text",
-      "text": {
-          "body": body
-        }
-    }
-  })
-  .then(res => res.json())
-}
+      preview_url: false,
+      recipient_type: "individual",
+      to: to,
+      type: "text",
+      text: {
+        body: body,
+      },
+    },
+  }).then((res) => res.json());
+};
