c4b9778a5da4080fbc56513171814f5dfeb47e2f
Simon de Haan <simon@praekelt.org>
2020-04-16 10:15:07 +0200

    use axios.client

--- send-message.js
+++ send-message.js
@@ -1,27 +1,35 @@
-const axios = require("axios").default;
+const axios = require("axios");
 const debug = require("debug")("turn");
 
 const TOKEN = process.env.TOKEN;
 
-const turnMsg = `https://whatsapp.turn.io/v1/messages`;
+// const turnMsg = `https://whatsapp.turn.io/v1/messages`;
 module.exports = function sendMessage(messageId, body, to) {
   debug(`sending message to ${to} in reply to ${messageId}`);
-  return axios({
-    method: "post",
-    url: turnMsg,
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-      "X-Turn-In-Reply-To": messageId,
-      "Content-Type": "application/json"
-    },
-    body: {
-      preview_url: false,
-      recipient_type: "individual",
-      to: to,
-      type: "text",
-      text: {
-        body: body
+
+  return axios
+    .create({
+      baseURL: "https://whatsapp.turn.io",
+      timeout: 300,
+      headers: { Authorization: `Bearer ${TOKEN}` },
+    })
+    .post(
+      "/v1/messages",
+      {
+        preview_url: false,
+        recipient_type: "individual",
+        to: to,
+        type: "text",
+        text: {
+          body: body,
+        },
+      },
+      {
+        headers: {
+          "X-Turn-In-Reply-To": messageId,
+          "Content-Type": "application/json",
+        },
       }
-    }
-  }).then(res => res.json());
+    )
+    .then((response) => response.json());
 };
