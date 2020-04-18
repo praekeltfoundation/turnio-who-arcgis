42ac961051347b865cb955460f33ea50c8183feb
Simon de Haan <simon@praekelt.org>
2020-04-18 17:19:44 +0200

    use morgan for request logging

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const inspect = require("./inspect");
 
 const { sendCountryDataBasedOnPhoneNumber } = require("./send-message");
@@ -6,7 +7,7 @@ const { sendCountryDataBasedOnPhoneNumber } = require("./send-message");
 const app = express();
 
 app.use(express.json());
-
+app.use(morgan("combined"));
 app.post("/stats", async (req, res) => {
   if (req.body.statuses) {
     const { recipient_id, status } = req.body.statuses[0];
