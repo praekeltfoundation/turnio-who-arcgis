7ea66de12ddafbd67bae64e54593abf09b16f55f
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-05-11 15:06:02 +0200

    Added test for ITA message formatting

--- __tests__/format-message.test.js
+++ __tests__/format-message.test.js
@@ -1,7 +1,7 @@
 const formatMsg = require("../format-message");
 
 describe("format-message tests", () => {
-  it("should enter the data in to the message based on country_code", () => {
+  it("should enter the data in to the ENG (default) template based on country_code", () => {
     const date = new Date();
     const mock_date = new Date('May 20, 2020 11:20:18')
     jest
@@ -10,6 +10,7 @@ describe("format-message tests", () => {
     const mockStatistic = {
       id: "1",
       country_code: "ZAF",
+      country_code_2: "ZA",
       updated: date,
       new_cases: "96",
       cum_cases: "1845",
@@ -53,5 +54,55 @@ https://covid19.who.int/
 📌 Reply 0 for Menu
 `;
     expect(formatMsg(mockStatistic, mockGlobal)).toEqual(formattedExample);
+  });
+  it("should enter the data in to the ITA template based on country_code and language", () => {
+    const date = new Date();
+    const mock_date = new Date('May 20, 2020 11:20:18')
+    jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mock_date)
+    const mockStatistic = {
+      id: "1",
+      country_code: "ZAF",
+      country_code_2: "ZA",
+      updated: date,
+      new_cases: "96",
+      cum_cases: "1845",
+      new_deaths: "5",
+      cum_deaths: "18",
+      createdAt: date,
+      updatedAt: date
+    };
+    const mockGlobal = {
+      id: "1",
+      country_code: "Global",
+      updated: date,
+      new_cases: "3000",
+      cum_cases: "70000",
+      new_deaths: "200",
+      cum_deaths: "4500",
+      createdAt: date,
+      updatedAt: date
+    };
+    const formattedExample = `*Ultimi casi* 🔢
+
+_Dati segnalati dalle autorità nazionali entro le 11:20 CET 20 maggio 2020_
+
+Totale (nuovi) casi nelle ultime 24 ore
+
+*Sudafrica*
+1845 confermati (96)
+18 morti (5)
+
+*A livello globale*
+70000 confermati (3000)
+4500 morti (200)
+
+*Cruscotto situazione romanzo di Coronavirus (COVID-19)*
+Questa dashboard / mappa interattiva fornisce gli ultimi numeri globali e numeri per paese di casi COVID-19 su base giornaliera.
+
+*Per i numeri dei paesi, trova gli ultimi rapporti sulla situazione qui:* https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports
+`;
+    expect(formatMsg(mockStatistic, mockGlobal, "ITA")).toEqual(formattedExample);
   });
 });
