88c9f41f26193465092b92027498f506687c3353
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-08-04 12:54:09 +0200

    Rip out the old tests and put halfway decent ones in

--- __tests__/retrieve-data.test.js
+++ __tests__/retrieve-data.test.js
@@ -1,33 +1,52 @@
 const axios = require("axios");
 const {
+  retrieveCountryData,
   retrieveCountryStatsFromArcGis,
+  retrieveGlobalStatsFromArcGis,
   shouldNotHaveToUpdate,
+  lessThanADayOld,
   retrieveContactLanguage
 } = require("../retrieve-data");
+const { Statistics } = require("../models");
 
+// Mock api calls
+jest.mock("axios");
 const mockAxios = jest.genMockFromModule('axios')
 mockAxios.create = jest.fn(() => mockAxios)
 
-mockAxios.mockImplementation(() =>
-  Promise.resolve(() => ({
-    features: [{ attributes: {} }]
-  }))
-);
-
-describe("retrieve data from cache or server", () => {
-  it("should try to fetch data from arcGIS", async done => {
+describe("fetch data from arcGIS APIs", () => {
+  it("should add the country to the country query", async () => {
+    axios.get.mockImplementation((url) => {
+      if (url.indexOf("NLD") >= 0 ) {
+        return Promise.resolve({ "data": {"message": "NLD present in url"}});
+      } else {
+        return Promise.resolve({ "data": {"message": "NLD not present in url"}});
+      }});
     const data = await retrieveCountryStatsFromArcGis("NLD");
-    done();
-  });
-});
-
-describe("retrieve data from cache or server", () => {
-  it("should try to fetch data from arcGIS", async done => {
-    const data = await retrieveCountryStatsFromArcGis("NLD");
-    done();
+    expect(data.message).toBe("NLD present in url");
   });
 
-  it("should not update if less than 8 hours ago", () => {
+  it("should add the date to the global query", async () => {
+    // Mocks
+    const mock_date = new Date('May 20, 2020 11:20:18')
+    const spy = jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mock_date)
+
+    axios.get.mockImplementation((url) => {
+      if (url.indexOf("5/20/2020") >= 0 ) {
+        return Promise.resolve({ "data": {"message": "Date present in url"}});
+      } else {
+        return Promise.resolve({ "data": {"message": "Date not present in url"}});
+      }});
+
+    const data = await retrieveGlobalStatsFromArcGis();
+
+    expect(data.message).toBe("Date present in url");
+    spy.mockRestore()
+  });
+
+  it("should not have to update if less than 1 hour old", () => {
     const date = new Date();
     const mock = {
       id: 1,
@@ -40,14 +59,14 @@ describe("retrieve data from cache or server", () => {
       createdAt: date,
       updatedAt: date
     };
-    const oldDate = new Date(new Date() - 30000000);
+    const oldDate = new Date(new Date() - 10000000);
     const oldMock = { ...mock, updatedAt: oldDate };
     expect(shouldNotHaveToUpdate(mock)).toBe(true);
     expect(shouldNotHaveToUpdate(oldMock)).toBe(false);
   });
 
-  it("should get data from the database", () => {
-    const date = new Date();
+  it("should only return true if the cache is less than a day old", () => {
+    const date = new Date(new Date() - (80000000));
     const mock = {
       id: 1,
       country_code: "ZAF",
@@ -59,6 +78,90 @@ describe("retrieve data from cache or server", () => {
       createdAt: date,
       updatedAt: date
     };
+    const oldDate = new Date(new Date() - (90000000));
+    const oldMock = { ...mock, updatedAt: oldDate };
+    expect(lessThanADayOld(mock)).toBe(true);
+    expect(lessThanADayOld(oldMock)).toBe(false);
+  });
+
+  it("should get cached data from the database", async () => {
+    const date = new Date();
+    const stats = await Statistics.create({
+      country_code: "ZAF",
+      updated: date,
+      new_cases: 96,
+      cum_cases: 1845,
+      new_deaths: 5,
+      cum_deaths: 18,
+      createdAt: date,
+      updatedAt: date
+    });
+
+    data = await retrieveCountryData("ZAF");
+    expect(data.cum_cases).toBe(stats.cum_cases)
+  });
+
+  it("should pull data from arcGIS if the cache is empty", async () => {
+    axios.get.mockImplementation((url) => {
+        return Promise.resolve({"data": {"features": [{"attributes": {
+          "ISO_2_CODE": "ZA",
+          "date_epicrv": 1596326400000,
+          "NewCase": 96,
+          "CumCase": 1845,
+          "NewDeath": 5,
+          "CumDeath": 18
+        }}]}});
+      });
+    data = await retrieveCountryData("ZAF");
+    expect(data.cum_cases).toBe(1845)
+  });
+
+  it("should use yesterdays cached stats if new case numbers is empty", async () => {
+    const date = new Date(new Date() - (90000000));
+    const stats = await Statistics.create({
+      country_code: "ZAF",
+      updated: date,
+      new_cases: 96,
+      cum_cases: 1845,
+      new_deaths: 5,
+      cum_deaths: 18,
+      createdAt: date,
+      updatedAt: date
+    });
+    axios.get.mockImplementation((url) => {
+        return Promise.resolve({"data": {"features": [{"attributes": {
+          "ISO_2_CODE": "ZA",
+          "date_epicrv": 1596326400000,
+          "NewCase": 0,
+          "CumCase": 8888,
+          "NewDeath": 0,
+          "CumDeath": 4444
+        }}]}});
+      });
+    data = await retrieveCountryData("ZAF");
+    expect(data.cum_cases).toBe(1845)
+  });
+
+  it("should use yesterdays stats from arcGIS if cache and new case numbers empty", async () => {
+    axios.get.mockImplementation((url) => {
+        return Promise.resolve({"data": {"features": [{"attributes": {
+          "ISO_2_CODE": "ZA",
+          "date_epicrv": 1596326400000,
+          "NewCase": 0,
+          "CumCase": 8888,
+          "NewDeath": 0,
+          "CumDeath": 4444
+        }}, {"attributes": {
+          "ISO_2_CODE": "ZA",
+          "date_epicrv": 1596240000000,
+          "NewCase": 96,
+          "CumCase": 1845,
+          "NewDeath": 5,
+          "CumDeath": 4444
+        }}]}});
+      });
+    data = await retrieveCountryData("ZAF");
+    expect(data.cum_cases).toBe(1845)
   });
 });
 
